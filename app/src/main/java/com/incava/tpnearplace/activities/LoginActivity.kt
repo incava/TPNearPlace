2a8ccd868269cf1928c274d536252fea88605d7d
incava <ingi1118@naver.com>
2023-03-29 11:25:37 +0900

    feat : 로그인 클릭이벤트 구현

--- app/src/main/java/com/incava/tpnearplace/activities/LoginActivity.kt
+++ app/src/main/java/com/incava/tpnearplace/activities/LoginActivity.kt
@@ -1,12 +1,47 @@
 package com.incava.tpnearplace.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.incava.tpnearplace.R
+import com.incava.tpnearplace.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvPlace.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+        binding.signUp.setOnClickListener {
+            startActivity(Intent(this,SignupActivity::class.java))
+        }
+        binding.linearLogin.setOnClickListener {
+            startActivity(Intent(this,SignupActivity::class.java))
+        }
+
+        binding.ivKakao.setOnClickListener { clickedLoginKakao() }
+        binding.ivGoogle.setOnClickListener {  clickedLoginGoogle() }
+        binding.ivNaver.setOnClickListener {  clickedLoginNaver() }
+
+
     }
+
+    fun clickedLoginKakao(){
+
+    }
+
+    fun clickedLoginGoogle(){
+
+    }
+
+    fun clickedLoginNaver(){
+
+    }
+
+
+
+
 }
