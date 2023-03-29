fca89c82934077ec4e63eae8dd5080258d2480c0
incava <ingi1118@naver.com>
2023-03-29 10:07:12 +0900

    feat : splash화면 기능 구현

--- app/src/main/java/com/incava/tpnearplace/activities/SplashActivity.kt
+++ app/src/main/java/com/incava/tpnearplace/activities/SplashActivity.kt
@@ -1,12 +1,20 @@
 package com.incava.tpnearplace.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.incava.tpnearplace.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,LoginActivity::class.java))
+            finish()
+        },1500)
     }
 }
