39c58787b180be4242d0e0ace5b659587ef7aed4
incava <ingi1118@naver.com>
2023-04-03 11:01:21 +0900

    feat : main 메뉴설정.

--- app/src/main/java/com/incava/tpnearplace/activities/MainActivity.kt
+++ app/src/main/java/com/incava/tpnearplace/activities/MainActivity.kt
@@ -3,20 +3,34 @@ package com.incava.tpnearplace.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.incava.tpnearplace.R
+import com.incava.tpnearplace.databinding.ActivityMainBinding
 import com.kakao.sdk.user.UserApiClient
 
 class MainActivity : AppCompatActivity() {
+    val binding : ActivityMainBinding by lazy{ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        UserApiClient.instance.unlink { error ->
-            if (error != null) {
-                Log.e("tag", "연결 끊기 실패", error)
-            }
-            else {
-                Log.i("tag", "연결 끊기 성공. SDK에서 토큰 삭제 됨")
-            }
-        }
+        setSupportActionBar(binding.toolbar)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_aa -> Toast.makeText(this,"aa",Toast.LENGTH_SHORT).show()
+            R.id.menu_bb -> Toast.makeText(this, "bb",Toast.LENGTH_SHORT).show()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
